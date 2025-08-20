1ca0b1216493d0e709c93cc86d85a10e354296d1
鳳陽合同会社 <japantopworld@gmail.com>
2025-08-21 01:43:51 +0900

    Update guide.py

--- routes/guide.py
+++ routes/guide.py
@@ -1,7 +1,9 @@
+# routes/guide.py
+
 from flask import Blueprint, render_template
 
 guide_bp = Blueprint('guide', __name__)
 
 @guide_bp.route("/guide")
-def guide():
+def guide_page():
     return render_template("pages/guide.html")
