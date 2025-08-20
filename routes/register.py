9ea0825d49bfa334d83cc9fb728b4a6fd8fdb384
鳳陽合同会社 <japantopworld@gmail.com>
2025-08-21 02:05:21 +0900

    Update register.py

--- routes/register.py
+++ routes/register.py
@@ -1,1 +1,9 @@
+# routes/register.py
+from flask import Blueprint, render_template
+
+register_bp = Blueprint('register', __name__)
+
+@register_bp.route("/register")
+def register():
+    return render_template("pages/register_worker.html")
 
