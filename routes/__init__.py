5b9dfb0e20d0d8baa766f06503a405bb2fd8884d
鳳陽合同会社 <japantopworld@gmail.com>
2025-08-20 18:43:53 +0900

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,3 +1,4 @@
-from . import dashboard
-from . import search
-from . import ranking
+# Blueprintを他ファイルで読み込めるようにする初期化用ファイル
+
+# 各ルートモジュールのBlueprint読み込み
+from . import dashboard, search, ranking, home
