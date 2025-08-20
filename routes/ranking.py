f512bb0197069975dad1f5d2dce003babc1f0a8d
鳳陽合同会社 <japantopworld@gmail.com>
2025-08-20 09:29:59 +0900

    Update ranking.py

--- routes/ranking.py
+++ routes/ranking.py
@@ -1,1 +1,8 @@
+from flask import Blueprint, render_template
+
+ranking_bp = Blueprint('ranking', __name__)
+
+@ranking_bp.route("/ranking")
+def ranking():
+    return render_template("pages/ranking.html")
 
