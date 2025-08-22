eefe11c52c3e52c590d20021a4689cf0989df215
鳳陽合同会社 <japantopworld@gmail.com>
2025-08-22 20:05:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, session, redirect, url_for
-from login import login_bp
-from register import register_bp
+from routes.login import login_bp
+from routes.register import register_bp
+from routes.home import home_bp
+from routes.health import health_bp
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"
@@ -8,14 +10,12 @@ app.secret_key = "your_secret_key"
 # Blueprint 登録
 app.register_blueprint(login_bp)
 app.register_blueprint(register_bp)
+app.register_blueprint(home_bp)
+app.register_blueprint(health_bp)
 
 @app.route("/")
-def home():
-    return render_template("pages/home.html")
-
-@app.route("/healthz")
-def healthz():
-    return "OK", 200
+def index():
+    return redirect(url_for("home_bp.home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
