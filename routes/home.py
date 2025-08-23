73103c62d044cdfdd9b917cc6ae822203609dbe0
鳳陽合同会社 <japantopworld@gmail.com>
2025-08-23 20:57:08 +0900

    Update home.py

--- routes/home.py
+++ routes/home.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session, redirect, url_for
 
 home_bp = Blueprint("home_bp", __name__)
 
-@home_bp.route("/home")
+@home_bp.route("/")
 def home():
-    user_name = session.get("user_name", "ゲスト")
-    return render_template("pages/home.html", user_name=user_name)
+    if "user_id" in session:
+        user_name = session.get("user_name", "ゲスト")
+        return render_template("pages/home.html", user_name=user_name)
+    else:
+        return render_template("pages/welcome.html")
