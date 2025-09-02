a843ea1a1033c94d648fc5d47d5c2bc8fb02c916
鳳陽合同会社 <japantopworld@gmail.com>
2025-09-02 19:44:00 +0900

    Update pages.py

--- routes/pages.py
+++ routes/pages.py
@@ -1,11 +1,7 @@
-# routes/pages.py
-
 from flask import Blueprint, render_template, session
 
-# Blueprint名を pages_bp に統一（home.htmlと一致）
-pages_bp = Blueprint("pages_bp", __name__, url_prefix="/mypage")
+pages_bp = Blueprint("pages_bp", __name__)
 
-@pages_bp.route("/")
-def mypage():
-    username = session.get("username", "ゲスト")
-    return render_template("pages/mypage.html", username=username)
+@pages_bp.route("/home")
+def home():
+    return render_template("pages/home.html")
