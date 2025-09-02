235a225100ac7298d39f9e5d9db2e067178bec1d
鳳陽合同会社 <japantopworld@gmail.com>
2025-09-02 19:44:28 +0900

    Update mypage.py

--- routes/mypage.py
+++ routes/mypage.py
@@ -4,8 +4,6 @@ mypage_bp = Blueprint("mypage_bp", __name__)
 
 @mypage_bp.route("/mypage")
 def mypage():
-    if "user_info" not in session:
-        return redirect(url_for("login_bp.login"))
-
-    user_info = session["user_info"]
-    return render_template("pages/mypage.html", user_info=user_info)
+    if 'user_id' not in session:
+        return redirect(url_for('login_bp.login'))
+    return render_template("pages/mypage.html")
