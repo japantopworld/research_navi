a96e3e3b6ce645370cf380e3ee55b5783e4488f6
鳳陽合同会社 <japantopworld@gmail.com>
2025-09-03 20:41:59 +0900

    Update mypage.py

--- routes/mypage.py
+++ routes/mypage.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template, session, redirect, url_for
+from flask import Blueprint, render_template, request
+from utils.profit_finder import get_profit_items
 
 mypage_bp = Blueprint("mypage_bp", __name__)
 
-@mypage_bp.route("/mypage")
+@mypage_bp.route("/mypage", methods=["GET", "POST"])
 def mypage():
-    if "user_id" not in session:
-        return redirect(url_for("login_bp.login"))
-    return render_template("pages/mypage.html")
+    min_profit_rate = float(request.form.get("min_profit_rate", 10))
+    max_profit_rate = float(request.form.get("max_profit_rate", 20))
+    items = get_profit_items(min_profit_rate, max_profit_rate)
+    return render_template("pages/mypage.html", items=items, min_rate=min_profit_rate, max_rate=max_profit_rate)
