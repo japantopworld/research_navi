8bc599fa31a7a27d3d6e1d82077205198b180c8e
鳳陽合同会社 <japantopworld@gmail.com>
2025-09-06 12:48:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-# main.py
-
 from flask import Flask
 from routes.search import search_bp
 from routes.home import home_bp
-from routes.ranking import ranking_bp       # ✅ 追加
+from routes.ranking import ranking_bp
 from routes.health_check import health_check_bp
+from routes.guide import guide_bp             # ✅ 追加
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -12,8 +11,9 @@ app.secret_key = 'your_secret_key'
 # Blueprint 登録
 app.register_blueprint(home_bp)
 app.register_blueprint(search_bp)
-app.register_blueprint(ranking_bp)          # ✅ 追加
+app.register_blueprint(ranking_bp)
 app.register_blueprint(health_check_bp)
+app.register_blueprint(guide_bp)             # ✅ 追加
 
 # Healthcheck用ルート（Render用）
 @app.route('/healthz')
