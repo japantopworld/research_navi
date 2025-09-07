25be8fa1f9091611868e0bae20b486c46e50370b
鳳陽合同会社 <japantopworld@gmail.com>
2025-09-08 01:43:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return '<h1>Hello Flask!</h1>'
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return "<h1>Register route direct from main.py</h1>"
