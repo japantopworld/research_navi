e8136845eba2f5d4555658bf38691cf997906796
鳳陽合同会社 <japantopworld@gmail.com>
2025-09-07 18:25:41 +0900

    Update login.py

--- routes/login.py
+++ routes/login.py
@@ -1,16 +1,3 @@
-from flask import Blueprint, redirect, url_for, session
-
-auth_bp = Blueprint("auth_bp", __name__)
-
 @auth_bp.route('/register', methods=['GET', 'POST'])
 def register():
     return "<h1>登録ページテスト（テンプレートなし）</h1>"
-
-@auth_bp.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('auth_bp.login'))
-
-@auth_bp.route('/login')
-def login():
-    return "<h1>ログインページ（仮）</h1>"
