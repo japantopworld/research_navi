9210adefe4e5d593171bf2831800d97f5cf27512
鳳陽合同会社 <japantopworld@gmail.com>
2025-09-16 08:22:50 +0900

    Create user.py

---
+++ models/user.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=False)
+
+    def to_dict(self):
+        return {
+            "username": self.username,
+            "password": self.password,
+            "email": self.email or ""
+        }
