f9446f74a41972f66dac85012bc4ffd6e962ec94
鳳陽合同会社 <japantopworld@gmail.com>
2025-09-17 18:22:51 +0900

    Add files via upload

---
+++ routes/general_admin.py
@@ -0,0 +1,10 @@
+# research_navi/routes/general_admin.py
+from flask import Blueprint, render_template, session, redirect, url_for
+
+general_admin_bp = Blueprint("general_admin_bp", __name__)
+
+@general_admin_bp.route("/admin/general")
+def general_admin():
+    if "user_id" not in session:
+        return redirect(url_for("login_bp.login"))
+    return render_template("pages/general_admin.html")
